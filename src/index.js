a812eb9c93c854316ae55b7db92acd8d3605e33e
Rotem10 <65648972+Rotem10@users.noreply.github.com>
2022-11-17 15:47:34 +0200

    focus update

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 function main() {
-  document.querySelector("#first-input").focus();
-  inputs = Array.from(document.querySelectorAll("input"));
-  inputs.map((input) => input.setAttribute("maxlength", "1"));
+  const inputs = Array.from(document.querySelectorAll("input"));
+  let id = 1;
+  inputs.forEach((input) => {
+    input.setAttribute("maxlength", "1");
+    input.setAttribute("id", id);
+    id++;
+    input.addEventListener("keyup", (event) => {
+      event.preventDefault();
+      input.value = event.target.value.toUpperCase();
+      const nextId = Number(event.target.id) + 1;
+      document.getElementById(nextId).focus();
+    });
+  });
+  document.getElementById("1").focus();
 }
 
 document.addEventListener("DOMContentLoaded", () => {
   main();
 });
-// input.setAttribute("maxlenght", 1)
